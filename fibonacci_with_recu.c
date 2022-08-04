149f9c4df63b0c1e9609a66450946d9e4d7a15b9
Rushi Lunagariya <75974714+Rushi7667@users.noreply.github.com>
2022-08-04 23:17:35 +0530

    added fibonacci without recursion

--- fibonacci_with_recu.c
+++ fibonacci_with_recu.c
@@ -20,3 +20,27 @@ int fibo(int n){
         return fibo(n-1) + fibo(n-2);
     }
 }
+
+//fibonacci without recursion
+
+#include <stdio.h>
+
+void main(){
+    int i,n,n1=0,n2=1;
+    int n3=n1+n2;
+    
+    printf("Enter the terms of fibonacci series : ");
+    scanf("%d", &n);
+    
+    printf("0\n1\n");
+
+
+    for (i=0;i<n-2;i++){
+        n3 = n1+n2;
+        n1=n2;
+        n2=n3;
+        
+        printf("%d\n",n3);
+    }
+    
+}
